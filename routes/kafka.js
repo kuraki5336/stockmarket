26eba38eb73408d45a95f06948026351a046fd56
Copi5336 <jason.han@tpisoftware.com>
2021-06-11 14:48:46 +0800

    kafka test

--- routes/kafka.js
+++ routes/kafka.js
@@ -2,16 +2,20 @@ var express = require("express");
 var router = express.Router();
 var kafka = require("kafka-node");
 var KeyedMessage = kafka.KeyedMessage;
-const client = new kafka.KafkaClient({ kafkaHost: "10.20.30.208:9092" });
-const producer = new kafka.Producer(client);
+const client = new kafka.KafkaClient({ kafkaHost: "10.20.30.208:9092, 10.20.30.208:9093, 10.20.30.208:9094" });
+const producer = new kafka.Producer(client, {
+  partitionerType: 1,
+  requireAcks: 1,
+  ackTimeoutMs: 100,
+});
 const km = new KeyedMessage("key", "message");
-const rep = [{ topic: "social", messages: "ready" }];
-
+const rep = { topic: "social", messages: 'init', key: 'system' }
 // 服務器啟動通知
 producer.on("ready", function () {
-  producer.send(rep, function (err, data) {
-    console.log(data);
-  });
+
+  // producer.send(rep, function (err, data) {
+  //   console.log(data);
+  // });
 });
 
 producer.on("error", function (err) {
@@ -26,6 +30,7 @@ const consumer = new kafka.Consumer(
   ],
   {
     autoCommit: false,
+
   }
 );
 
@@ -40,7 +45,11 @@ consumer.on("error", function (err) {
 /* GET users listing. */
 router.get("/1/:xmessage", function (req, res, next) {
   const payload = [
-    { topic: "social", messages: req.params.xmessage},
+    {
+      topic: "social",
+      messages: req.params.xmessage,
+      key: 'theKey'
+    },
   ];
   producer.send(payload, function (err, data) {
     res.send(`user: ` + req.params.xmessage);
